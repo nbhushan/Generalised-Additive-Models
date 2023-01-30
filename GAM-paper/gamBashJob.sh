0f1dbd7d2b08970480e641b15d4135c2cd84750b
Nitin Bhushan <bhushan.nitin@posteo.net>
2023-01-30 23:16:17 +0100

    Update and rename gamFinalJob.sh to gamBashJob.sh

--- GAM-paper/gamBashJob.sh
+++ GAM-paper/gamBashJob.sh
@@ -2,7 +2,7 @@
 #SBATCH --time=7-00:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
-#SBATCH --job-name=gam_final_p275030
+#SBATCH --job-name=gam_p275030
 #SBATCH --mem=120GB
 #SBATCH --mail-user=n.bhushan@rug.nl
 #SBATCH --mail-type=BEGIN,END,FAIL
@@ -10,4 +10,4 @@
 pwd
 module load R
 cd $HOME/GAMM/
-Rscript gamFinalscript.r
+Rscript gamFitscript.r
